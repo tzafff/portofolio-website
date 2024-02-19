642398716cfc8a02b3961a65f158704d6912bbb7
tzafff <christzaf99@gmail.com>
2024-02-19 23:49:07 +0200

    NavBar/Header

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,14 @@
 import Image from "next/image";
 import HeroSection from "@/app/components/HeroSection";
+import NavBar from "@/app/components/NavBar";
 
 export default function Home() {
   return (
       <main className="flex min-h-screen flex-col bg-[#121212]">
           <div className="container mt-24 mx-auto px-12 py-4">
+              <NavBar />
               <HeroSection />
+
           </div>
 
     </main>
