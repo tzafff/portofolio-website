b3b8b9ed1e55e85d1172f541695ce8c0d32b99f6
tzafff <christzaf99@gmail.com>
2024-02-20 19:24:23 +0200

    Update About Section

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -9,12 +9,14 @@ const TAB_DATA = [
         id: "skills",
         content: (
             <ul className="list-disc pl-2">
-                <li>Node.js</li>
-                <li>Express</li>
-                <li>PostgreSQL</li>
-                <li>Sequelize</li>
-                <li>JavaScript</li>
+                <li>Java</li>
+                <li>Spring/SpringBoot</li>
+                <li>Elastic Search</li>
+                <li>JavaScript/TypeScript</li>
+                <li>Angular</li>
                 <li>React</li>
+                <li>Solidity</li>
+                <li>EthersJS/Web3JS</li>
             </ul>
         ),
     },
@@ -23,8 +25,9 @@ const TAB_DATA = [
         id: "education",
         content: (
             <ul className="list-disc pl-2">
-                <li>Fullstack Academy of Code</li>
-                <li>University of California, Santa Cruz</li>
+                <li>1st Unified High School of Elefsina, Athens</li>
+                <li>Department of Informatics and Telecommunications
+                    at University of the Peloponnese</li>
             </ul>
         ),
     },
@@ -33,8 +36,9 @@ const TAB_DATA = [
         id: "certifications",
         content: (
             <ul className="list-disc pl-2">
-                <li>AWS Cloud Practitioner</li>
-                <li>Google Professional Cloud Developer</li>
+                <li>Ethereum Blockchain Developer Bootcamp With Solidity (2023) - Udemy</li>
+                <li>HyperLedger Fabric 2.x Network Design & Setup - Udemy</li>
+                <li>Ethereum and Solidity: The Complete Developer's Guide - Udemy</li>
             </ul>
         ),
     },
@@ -53,14 +57,13 @@ const AboutSection = () => {
     return (
         <section className="text-white" id="about">
             <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
-                <Image src="/images/about-image.png" width={500} height={500} />
+                <Image src="/images/about.png" width={1000} height={1000} />
                 <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
                     <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
                     <p className="text-base lg:text-lg">
                         I am a full stack web developer with a passion for creating
                         interactive and responsive web applications. I have experience
-                        working with JavaScript, React, Redux, Node.js, Express, PostgreSQL,
-                        Sequelize, HTML, CSS, and Git. I am a quick learner and I am always
+                        working with Spring, SpringBoot, Elastic Search, JavaScript, TypeScript React, Angular, Solidity, EthersJs, Web3JS and Git. I am a quick learner and I am always
                         looking to expand my knowledge and skill set. I am a team player and
                         I am excited to work with others to create amazing applications.
                     </p>
